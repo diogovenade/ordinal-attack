Added Gradient Sign Attack and classes for metrics, bug fixes

test.py
@@ -2,12 +2,17 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('dataset')
 parser.add_argument('model_path')
+parser.add_argument('output')
+parser.add_argument('--attack', type=str, default=None)
+parser.add_argument('--epsilon', type=float, default=0.1)
 args = parser.parse_args()
 
 import torch
 import data
+from mymetrics import OneOff, MeanAbsoluteError, Unimodality 
 from torcheval.metrics import MulticlassAccuracy
 from torchvision.transforms import v2
+from advertorch import attacks
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -26,24 +31,38 @@ model.to(device)
 model.eval()
 
 accuracy = MulticlassAccuracy(num_classes=num_classes).to(device)
-one_off = 0
-mae_sum = 0
+one_off = OneOff()
+mae = MeanAbsoluteError()
+unimodality = Unimodality()
 
-total = 0
+adversary = None
+if args.attack:
+    adversary = getattr(attacks, args.attack)(model, lambda pred, true: model.loss(pred, true).sum(), eps=args.epsilon)
 
 with torch.no_grad():
     for images, labels in dataloader:
         images = images.to(device)
         labels = labels.to(device)
+
+        if adversary:
+            with torch.enable_grad():
+                images.requires_grad = True
+                images = adversary.perturb(images, labels)
+
         preds = model(images)
         preds = preds.argmax(dim=1)
 
         accuracy.update(preds, labels)
-        one_off += ((preds == labels) | (torch.abs(preds - labels) == 1)).sum().item()
-        mae_sum += torch.abs(preds - labels).sum().item()
+        one_off.update(preds, labels)
+        mae.update(preds, labels)
+        unimodality.update(preds)
 
-        total += labels.size(0)
+results = (
+    f'Accuracy: {accuracy.compute().item():.4f}\n'
+    f'One-off Accuracy: {one_off.compute():.4f}\n'
+    f'Mean Absolute Error (MAE): {mae.compute():.4f}\n'
+    f'Unimodality: {unimodality.compute():.4f}\n'
+)
 
-print(f'Accuracy: {accuracy.compute().item():.4f}')
-print(f'One-off Accuracy: {one_off / total:.4f}')
-print(f'Mean Absolute Error (MAE): {mae_sum / total:.4f}')
+with open(args.output, 'w') as f:
+    f.write(results)